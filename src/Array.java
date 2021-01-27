f496639a10a77b542841755d7d403b5ec106e865
Dimpal <dimpal.agrawal_cs19@gla.ac.in>
2021-01-27 11:06:23 +0530

    first commit

--- src/Array.java
+++ src/Array.java
@@ -5,6 +5,24 @@
  *   File: Array.java
  */
 
-public class Array {
+import java.util.Arrays;
 
+public class Array {
+    public static void main(String[] args) {
+        int a[] = {7,3,6,4,5};
+
+        Arrays.sort(a);
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]);
+        }
+
+        int c = a[a.length-1];
+        int a1 = a[0];
+        int b1 = c;
+        int q = b1-a1;
+
+        System.out.println(" The differnce between both largest nad smallest numbers ");
+        System.out.print(q);
+
+    }
 }
---
+++ src/DuplicateElements.java
@@ -0,0 +1,10 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Dimpal Agrawal
+ *   Date: 1/27/2021
+ *   Time: 11:05 AM
+ *   File: DuplicateElements.java
+ */
+
+public class DuplicateElements {
+    
+}
